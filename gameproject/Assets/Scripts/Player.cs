f9ff213e05e372cf5da638ad1c8eba0d9c449d3f
806hyogi <rhkdgur120@naver.com>
2022-11-09 00:08:17 +0900

    2번째 영상까지 완료
    2022.11.09 현재개발 완료 부분
    맵, 플레이어 이동, 점프, 회피

--- gameproject/Assets/Scripts/Player.cs
+++ gameproject/Assets/Scripts/Player.cs
@@ -12,11 +12,12 @@ public class Player : MonoBehaviour
     bool wDown;
     bool jDown;
 
-    bool isjump; //점프 한계설정
-
+    bool isJump; //점프 한계설정
+    bool isDodge; //회피
 
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
     Rigidbody rigid; //물리효과를 내기위해
     Animator anim; 
@@ -40,6 +41,7 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Jump();
+        Dodge();
     }
 
     void GetInput()
@@ -47,12 +49,15 @@ public class Player : MonoBehaviour
         hAxis = Input.GetAxisRaw("Horizontal"); // Axis값을 정수로 반환하는 함수(오른쪽왼쪽 컨트롤)
         vAxis = Input.GetAxisRaw("Vertical"); //(위아래 컨트롤)
         wDown = Input.GetButton("Walk"); //shift 누른 상태에서만 걷게됨
-        jDown = Input.GetButtonDown("Walk"); //누른즉시 점프
+        jDown = Input.GetButtonDown("Jump"); //누른즉시 점프
     }
 
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized; //동시에 눌렀을때 1로 변환
+
+        if (isDodge)
+            moveVec = dodgeVec; // 회피를 하고 있을경우 움직임 벡터 -> 회피방향 벡터로 바뀌는 코드(회피하면서 다른 방향키 사용불가)
 
         if (wDown)
             transform.position += moveVec * speed * 0.3f * Time.deltaTime;
@@ -75,13 +80,42 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if (jDown && !isjump)  // 점프 한계설정
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge)  // 점프 한계설정  (움직임이 zero일때 그냥 점프, 액션 도중에 다른 액션 실행되지 않게 설계)
         {
-            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse); //점프 파워 설정
-            isjump = true;
+            rigid.AddForce(Vector3.up * 18, ForceMode.Impulse); //점프 파워 설정
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
         }
     }
 
-    OnColl
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)  // 점프 한계설정 (움직임이 zero가 아닐때 "회피(Dodge)")
+        {
+            dodgeVec = moveVec; //무브벡터를 닷지벡터에 대입
+            speed *= 2; //스피드 속도 두배
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f); // 닷지할때 0.4초의 시간차(딜레이)를 줌 <바로 닷지 안돼게 방지 코드>
+
+        }
+    }
+    
+    void DodgeOut() //시간차를 줘서 isDodge를 false
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    void OnCollisionEnter(Collision collision) //이벤트 함수로 착지 구현
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false; //바닥에 닿으면 점프를 할 수 있음
+        }
+    }
 
 }
