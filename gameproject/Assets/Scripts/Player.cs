e48c9936c868b485cde8ba3cafcad141275ebbff
806hyogi <rhkdgur120@naver.com>
2022-11-15 02:07:39 +0900

    드랍무기 입수와 교체
    영상 4편 완료

--- gameproject/Assets/Scripts/Player.cs
+++ gameproject/Assets/Scripts/Player.cs
@@ -6,22 +6,43 @@ public class Player : MonoBehaviour
 {
 
     public float speed;
+    public GameObject[] weapons; //플레이어 무기관련 변수
+    public bool[] hasWeapons; //플레이어 무기관련 변수
+
+    //아이템 변수 선언
+    public int ammo;
+    public int coin;
+    public int health;
+    public int hasGrenades;
+    //각 수치의 최대값을 저장할 변수 생성
+    public int maxAmmo;
+    public int maxCoin;
+    public int maxHealth;
+    public int maxHasGrenades;
 
     float hAxis; //input Axis 값을 받을 전역변수 선언
     float vAxis;
+
     bool wDown;
     bool jDown;
+    bool iDown;
+    bool sDown1; //무기교체 1번장비
+    bool sDown2; //무기교체 2번장비
+    bool sDown3; //무기교체 3번장비
 
     bool isJump; //점프 한계설정
     bool isDodge; //회피
-
+    bool isSwap; //무기교체동안에 아무것도 못하게함
 
     Vector3 moveVec;
     Vector3 dodgeVec;
 
     Rigidbody rigid; //물리효과를 내기위해
-    Animator anim; 
+    Animator anim;
 
+    GameObject nearObject; //트리거 된 아이템을 저장하기 위한 변수 선언
+    GameObject equipWeapon; //기존에 장착된 무기를 저장하는 변수를 선언, 활용
+    int equipWeaponIndex = -1;
 
     void Awake()
     {
@@ -30,11 +51,7 @@ public class Player : MonoBehaviour
     }
 
 
-    void Start()
-    {
-        
-    }
-
+   
     void Update()
     {
         GetInput();
@@ -42,6 +59,8 @@ public class Player : MonoBehaviour
         Turn();
         Jump();
         Dodge();
+        Swap();
+        Interation();
     }
 
     void GetInput()
@@ -50,6 +69,10 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical"); //(위아래 컨트롤)
         wDown = Input.GetButton("Walk"); //shift 누른 상태에서만 걷게됨
         jDown = Input.GetButtonDown("Jump"); //누른즉시 점프
+        iDown = Input.GetButtonDown("Interation"); //e키를 누르면 iDown 활성화됨 (Edit -> project setting)
+        sDown1 = Input.GetButtonDown("Swap1"); //무기 1번키를 받음
+        sDown2 = Input.GetButtonDown("Swap2"); //무기 2번키를 받음
+        sDown3 = Input.GetButtonDown("Swap3"); //무기 3번키를 받음
     }
 
     void Move()
@@ -58,14 +81,15 @@ public class Player : MonoBehaviour
 
         if (isDodge)
             moveVec = dodgeVec; // 회피를 하고 있을경우 움직임 벡터 -> 회피방향 벡터로 바뀌는 코드(회피하면서 다른 방향키 사용불가)
+        
+        if (isSwap) //무기 교체시 움직임도 멈춤
+            moveVec = Vector3.zero;
 
         if (wDown)
             transform.position += moveVec * speed * 0.3f * Time.deltaTime;
         else
             transform.position += moveVec * speed * Time.deltaTime; //shift 누르면 속도 느려짐
 
-
- 
 
         anim.SetBool("isRun", moveVec != Vector3.zero); //뛰는 상태는 0이 아니면 뜀
         anim.SetBool("isWalk", wDown); //걷는 상태 구현
@@ -80,7 +104,7 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge)  // 점프 한계설정  (움직임이 zero일때 그냥 점프, 액션 도중에 다른 액션 실행되지 않게 설계)
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge && !isSwap)  // 점프 한계설정  (움직임이 zero일때 그냥 점프, 액션 도중에 다른 액션 실행되지 않게 설계)
         {
             rigid.AddForce(Vector3.up * 18, ForceMode.Impulse); //점프 파워 설정
             anim.SetBool("isJump", true);
@@ -91,7 +115,7 @@ public class Player : MonoBehaviour
 
     void Dodge()
     {
-        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)  // 점프 한계설정 (움직임이 zero가 아닐때 "회피(Dodge)")
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge && !isSwap)  // 점프 한계설정 (움직임이 zero가 아닐때 "회피(Dodge)")
         {
             dodgeVec = moveVec; //무브벡터를 닷지벡터에 대입
             speed *= 2; //스피드 속도 두배
@@ -109,6 +133,56 @@ public class Player : MonoBehaviour
         isDodge = false;
     }
 
+    void Swap() //무기 교체 함수
+    {
+        if (sDown1 && (!hasWeapons[0] || equipWeaponIndex == 0)) //0을 중복해서 누르는것
+            return; //실행하면안됌
+        if (sDown2 && (!hasWeapons[1] || equipWeaponIndex == 1)) //1을 중복해서 누르는것
+            return; //실행하면안됌
+        if (sDown3 && (!hasWeapons[2] || equipWeaponIndex == 2)) //2을 중복해서 누르는것
+            return; //실행하면안됌
+
+        int weaponIndex = -1;
+        if (sDown1) weaponIndex = 0; //1번키 누르면 인덱스 0
+        if (sDown2) weaponIndex = 1; //2번키 누르면 인덱스 1
+        if (sDown3) weaponIndex = 2; //3번키 누르면 인덱스 2
+
+        if ((sDown1 || sDown2 || sDown3) && !isJump && !isDodge){ //점프나 회피중에는 무기를 못바꾸도록 
+            if (equipWeapon != null) //빈손일 경우는 실행 하지 않음
+                equipWeapon.SetActive(false);
+
+            equipWeaponIndex = weaponIndex;
+            equipWeapon = weapons[weaponIndex];
+            equipWeapon.SetActive(true);
+
+            anim.SetTrigger("doSwap"); //애니메이터 셋팅
+
+            isSwap = true; //교체동안에 아무것도 못하게 함
+
+            Invoke("SwapOut", 0.4f);
+        }
+    }
+
+    void SwapOut() //교체 시간이 끝나고 다시 다른 작업을 할수 있도록 함
+    {
+        isSwap = false;
+    }
+    
+    void Interation()
+    {
+        if(iDown && nearObject != null && !isJump && !isDodge) //플레이어 근처에 물건이 있다, 점프나 회피때 상호작용 못하게 막음.
+        {
+            if(nearObject.tag == "Weapon") //무기를 가지고 있다
+            {
+                Item item = nearObject.GetComponent<Item>();
+                int weaponIndex = item.value;
+                hasWeapons[weaponIndex] = true; //무기 인덱스를 트루로 바꿈
+
+                Destroy(nearObject); //아이템 정보를 가져와서 해당 무기 입수를 체크
+            }
+        }
+    }
+    
     void OnCollisionEnter(Collision collision) //이벤트 함수로 착지 구현
     {
         if(collision.gameObject.tag == "Floor")
@@ -118,4 +192,16 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnTriggerStay(Collider other) //트리거 이벤트
+    {
+        if (other.tag == "Weapon") //weapon 태그를 조건으로 하여 로직 작성
+            nearObject = other.gameObject; //near오브젝트에 저장
+        Debug.Log(nearObject.name);
+    }
+
+    void OnTriggerExit(Collider other) //트리거 이벤트
+    {
+        if (other.tag == "Weapon") //영역에서 벗어났을때 널값을 줘서 비움
+            nearObject = null;
+    }
 }
