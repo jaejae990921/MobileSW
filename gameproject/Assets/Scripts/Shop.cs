130b192aae88caeb6ce309683a91863cc9782fd6
sphy1597 <sphy1597@naver.com>
2022-12-06 19:06:08 +0900

    Stage일부 남음
    스테이지 일부 남음(몬스터)
    상점 일부 구현 ( 이동속도 )

--- gameproject/Assets/Scripts/Shop.cs
+++ gameproject/Assets/Scripts/Shop.cs
@@ -1,13 +1,14 @@
 using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
     public RectTransform uiGroup;
     public GameObject[] itemObj;
-
+    public GameManager manager;
 
     Player enterPlayer;
 
@@ -17,15 +18,31 @@ public class Shop : MonoBehaviour
         uiGroup.anchoredPosition = Vector3.zero;
     }
 
+    public void Buy(int index)
+    {
+        switch (index) {
+            case 6:
+                enterPlayer.speed *= 2; // 이동속도
+                break;
+            default:
+                break;
+        
+        }
+
+
+
+        Exit();
+        enterPlayer.isShop = false;
+    }
+
     public void Exit()
     {
         uiGroup.anchoredPosition = Vector3.down * 1000;
+        manager.StageStart();
+     
     }
 
-    public void Buy(int index)
-    {
-
-    }
+    
 
 
 }
