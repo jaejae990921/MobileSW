116aaf2792cbaeeebd7ba678b25d8565c977e217
sphy1597 <101171867+sphy1597@users.noreply.github.com>
2022-12-18 17:42:25 +0900

    StartZone.cs 수정 by gyu
    주석 수정

--- gameproject/Assets/Scripts/StartZone.cs
+++ gameproject/Assets/Scripts/StartZone.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartZone : MonoBehaviour
+public class StartZone : MonoBehaviour // 라운드 진행 테스트를 위한 스크립트
 {
 
     public GameManager manager;
